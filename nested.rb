0355ca8661edfc510665f1f13f01fbc8125e0af9
Ryan Odegard <rsodegard@gmail.com>
2019-09-25 22:46:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -22,7 +22,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:alan_kay][:known_for]
+	
 	
 	programmer_hash = 
  		{
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+     }programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
